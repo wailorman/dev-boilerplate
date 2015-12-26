bd0e7460aa657778376aa39fe9f70ba6c651ab73
wailorman <wailorman@gmail.com>
2015-12-26 23:06:22 +0300

    Remove unnecessary old files

--- entry.js
+++
@@ -1,4 +0,0 @@
-import welcome from './welcome';
-import './style.less'
-
-welcome(`John, Sally`);
--- welcome.js
+++
@@ -1,4 +0,0 @@
-module.exports = (arg) => {
-    //debugger;
-    console.log(`Welcome ${arg} !`);
-};
